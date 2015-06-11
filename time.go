4fda7875c55fc97de1b97b62c0246b63b5ccc119
albert <lomoalbert@gmail.com>
2015-06-11 22:33:21 +0800

    add examination

--- time.go
+++ time.go
@@ -18,6 +18,7 @@ Minute = 60 * Second                (60*1e9)
 Hour = 60 * Minute                  (60*60*1e9)
 */
 func main() {
+    starttime := time.Now()
     //time.After返回一个chan,指定时间后通过chan传回即时时间
     fmt.Println(<-time.After(2*time.Second))
     //time.Sleep 阻塞指定时间
@@ -26,7 +27,9 @@ func main() {
     //合法的单位是"ns", "us" , "ms", "s", "m", "h"。返回Duration值
     d, err := time.ParseDuration("1h2m3s4ms5.8us6ns")
     fmt.Println(d, err)
-
+    //返回从时间t到当前时间的间隔，time.Now().Sub(t)的简写
+    fmt.Println(time.Now().Sub(starttime))
+    fmt.Println(time.Since(starttime))
 
     //time.Tick 类似time.After,但可以持续使用
     c := time.Tick(1 * time.Second)
