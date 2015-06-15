8f5a0bfb896c544db065df3d84693e41259be24e
albert <lomoalbert@gmail.com>
2015-06-15 22:34:59 +0800

    add examination

--- io_ioutil_Readall.go
+++ io_ioutil_Readall.go
@@ -4,6 +4,20 @@ import "fmt"
 import "io/ioutil"
 import "strings"
 
+/*
+func ReadAll(r io.Reader) ([]byte, error)
+参数列表
+
+r 读取对象
+返回值：
+
+[]byte 返回读取对象的内容
+error 返回读取是否成功
+功能说明：
+
+本函数主要是用来从r读取直到遇到error或EOF并返回读取的数据；成功的读取返回的err为nil，而不是EOF。因为ReadAll定义为从资源读取数据直到EOF，它不会将从r读取的EOF视为应该报告的错误
+*/
+
 func main() {
     s := strings.NewReader("hello world!")
     b, e := ioutil.ReadAll(s)
---
+++ io_ioutil_readdir.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+import "io/ioutil"
+
+/*
+func ReadDir(dirname string) ([]os.FileInfo, error)
+参数列表
+
+dirname 读取目录的目录路径
+返回值：
+
+[]os.FileInfo 返回指定dirname目录下的一个有序的子目录信息列表
+error 返回读取是否成功
+功能说明：
+
+本函数主要是用来读取指定dirname目录，并返回一个有序的子目录信息列表
+
+*/
+func main() {
+    dir_list, e := ioutil.ReadDir("d:/goTest")
+    if e != nil {
+        fmt.Println("read dir error")
+        return
+    }
+    for i, v := range dir_list {
+        fmt.Println(i, "=", v.Name())
+    }
+}
