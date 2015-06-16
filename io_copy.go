871ce68ab330afe79a1b06bcace1a837124c12bc
albert <lomoalbert@gmail.com>
2015-06-16 23:20:12 +0800

    add examination

--- io_copy.go
+++ io_copy.go
@@ -4,7 +4,22 @@ import (
     "fmt"
     "os"
 )
+/*
+func Copy(dst Writer, src Reader) (written int64, err error)
+参数：
 
+dst 拷贝目标写入器
+src 拷贝源读取器
+返回值：
+
+written 拷贝字节数
+err 拷贝是否成功；nil代表拷贝成功
+功能说明： 向dst拷贝src的全部数据；读取src中数据直到EOF，故不会返回io.EOF
+
+可能的异常： io.ErrShortWrite:写入数据不等于读取数据
+
+示例： package main
+*/
 func main() {
     srcFile, _ := os.Open("copySrc.txt")
     destFile, _ := os.Create("copyDest.txt")
