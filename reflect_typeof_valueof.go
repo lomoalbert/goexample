74d533fd79426d0da9f4fb2acf10c479b176f56f
albert <lomoalbert@gmail.com>
2015-06-20 20:01:50 -0700

    add tail

---
+++ reflect_typeof_valueof.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    "fmt"
+    "reflect"
+)
+
+type MyStruct struct {
+    name string
+}
+
+func (this *MyStruct)GetName() string {
+    return this.name
+}
+
+func main() {
+    s := "this is string"
+    fmt.Println(reflect.TypeOf(s))
+    fmt.Println("-------------------")
+
+    fmt.Println(reflect.ValueOf(s))
+    var x float64 = 3.4
+    fmt.Println(reflect.ValueOf(x))
+    fmt.Println("-------------------")
+
+    a := new(MyStruct)
+    a.name = "yejianfeng"
+    typ := reflect.TypeOf(a)
+
+    fmt.Println(typ.NumMethod())
+    fmt.Println("-------------------")
+
+    b := reflect.ValueOf(a).MethodByName("GetName").Call([]reflect.Value{})
+    fmt.Println(b[0])
+
+}
+
+/*
+/tmp/reflect_typeof_valueof.go0go
+string
+-------------------
+this is string
+<float64 Value>
+-------------------
+1
+-------------------
+yejianfeng
+*/
