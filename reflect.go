c3247465d1e0279e0657c9570561f5e479de901e
albert <lomoalbert@gmail.com>
2015-06-20 19:43:42 -0700

    add tail

--- reflect.go
+++ reflect.go
@@ -1,13 +1,27 @@
 package main
 import "fmt"
-import "reflect"
+import (
+    "reflect"
+    "strconv"
+)
 func hello() {
     fmt.Println("hello world!")
 }
 
+func prints(i int) string {
+    fmt.Println("i =", i)
+    return strconv.Itoa(i)
+}
+
 func main() {
+    fv := reflect.ValueOf(prints)
+    params := make([]reflect.Value, 1)  //参数
+    params[0] = reflect.ValueOf(20)   //参数设置为20
+    rs := fv.Call(params)              //rs作为结果接受函数的返回值
+    fmt.Println("result:", rs[0].Interface().(string)) //当然也可以直接是rs[0].Interface()
+
     hl := hello
-    fv := reflect.ValueOf(hl)
-    fmt.Println("fv is reflect.Func ?", fv.Kind() == reflect.Func)
-    fv.Call(nil)
+    fv1 := reflect.ValueOf(hl)
+    fmt.Println("fv1 is reflect.Func ?", fv1.Kind() == reflect.Func)
+    fv1.Call(nil)
 }
