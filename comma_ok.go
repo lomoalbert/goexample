d948aca561f0b225429db377e131103e77d380b5
albert <lomoalbert@gmail.com>
2015-06-22 23:39:28 +0800

    add tail

--- comma_ok.go
+++ comma_ok.go
@@ -13,4 +13,22 @@ func main() {
     value = val.(string)
     fmt.Println(value)
     //fmt.Println(val.(int))   //将出现程序错误
+
+
+    type Html []interface{}
+
+    html := make(Html, 5)
+    html[0] = "div"
+    html[1] = "span"
+    html[2] = []byte("script")
+    html[3] = "style"
+    html[4] = "head"
+    for index, element := range html {
+        if value, ok := element.(string); ok {
+            fmt.Printf("html[%d] is a string and its value is %s\n", index, value)
+        } else if value, ok := element.([]byte); ok {
+            fmt.Printf("html[%d] is a []byte and its value is %s\n", index, string(value))
+        }
+    }
+
 }
