2c2effd946b7a1eb8f064843699566e69f75a31f
albert <lomoalbert@gmail.com>
2015-06-24 23:33:10 +0800

    add pizza

--- struct03.go
+++ struct03.go
@@ -2,27 +2,25 @@ package main
 import (
     "fmt"
 )
-//struct的匿名字段 即继承
+//struct的方法
 //struct不仅可以使用struct作为匿名字段，自定义类型、内置类型都可以作为匿名字段,而且可以在相应字段上做函数操作
 type person struct {
     name string
     age  int
 }
 
-type student struct {
-    person // 匿名函数,实现继承person结构
-    school string
+func (p person) growup01() {
+    p.age+=1
+    fmt.Println(p)
+}
+
+func (p *person) growup02() {
+    p.age+=1
+    fmt.Println(p)
 }
 
 func main() {
-    mark := student{person{"mark", 25}, "Computer Science"}
+    mark := person{"mark", 25}
+    mark.growup01()
     fmt.Println(mark)
-
-    mark.school="high school"
-    mark.person=person{"mark", 26}
-    mark.person.age += 1
-    fmt.Println(mark)
-
-
-
 }
