7510d45694fb0f04210b4510927577bb38db2092
albert <lomoalbert@gmail.com>
2015-06-25 23:25:24 +0800

    add pizza

--- struct03.go
+++ struct03.go
@@ -17,7 +17,6 @@ func (p person) growup01() {
     fmt.Println(p)
 }
 
-
 //如果一个method的receiver是*T，调用时，可以传递一个T类型的实例变量V，而不必用&V去调用这个method
 func (p *person) growup02() {
     p.age+=1
@@ -28,6 +27,10 @@ func main() {
     mark := person{"mark", 25}
     mark.growup01()     //{mark 26}
     fmt.Println(mark)   //{mark 25}
+    (&mark).growup01()  //{mark 26}
+    fmt.Println(mark)   //{mark 25}
     mark.growup02()     //&{mark 26}
+    fmt.Println(mark)   //{mark 26}
+    (&mark).growup02()     //&{mark 26}
     fmt.Println(mark)   //{mark 26}
 }
