b51f080d2c1b806c2a1b3e76aece2b2d56f92a8b
albert <lomoalbert@gmail.com>
2015-06-28 00:00:17 +0800

    add new make

--- for_range.go
+++ for_range.go
@@ -5,15 +5,15 @@ import "fmt"
 func main() {
     //list slice map 均可使用for range
     list := [...]rune{'a', 'b', 'c'}
-    //i:v 可以获取序号和值
+    //i:v 可以获取索引和值
     for i, v := range list {
         fmt.Println(i, v)
     }
-    //可以只获取序号,然后在循环内获取值
+    //可以只获取索引,然后在循环内获取值
     for i := range list {
         fmt.Println(i, list[i])
     }
-    //也可以不获取序号和值,仅仅循环len()次
+    //也可以不获取索引和值,仅仅循环len()次
     for range list {
         fmt.Println("*")
     }
