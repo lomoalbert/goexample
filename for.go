58a13cb353ca60349845ef9acd1960973d5a6ec2
albert <lomoalbert@gmail.com>
2015-06-27 22:34:17 +0800

    add new make

--- for.go
+++ for.go
@@ -21,4 +21,8 @@ func main() {
         fmt.Println("i现在的值为：", i)
     }
     fmt.Println("i最终的值为：", i)
+
+    for i, j, s := 0, 5, "a"; i < 3 && j < 100 && s != "aaaaa"; i, j, s = i+1, j+1, s + "a" {
+        fmt.Printf("i=%d, j=%d, s=%s\n", i, j, s)
+    }
 }
