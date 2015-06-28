60bfd28118574e4419f8192a78393dabd3a5353c
albert <lomoalbert@gmail.com>
2015-06-28 23:03:36 +0800

    add new make

--- goroutine.go
+++ goroutine.go
@@ -4,7 +4,12 @@ import "time"
 //goroutine使用go便捷地实现多协程
 
 func wait(c chan bool, i int) {
-    time.Sleep(time.Second)
+    time.Sleep(time.Second*i)
     c <- true
 }
 
+func main() {
+    var c chan bool
+    go wait(c, 5)
+    <-c
+}
