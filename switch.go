3f2e3a043de757c3ff86a16f4a8e21374c037dba
albert <lomoalbert@gmail.com>
2015-06-29 22:57:42 +0800

    add switch

--- switch.go
+++ switch.go
@@ -2,7 +2,7 @@ package main
 import "fmt"
 //switch是最灵活的一种控制语句,表达式可以不是常量或者字符串，也可以沒有表达式，如果没有表达式则如同if-else-else。
 //一般用法:和其他语言的Switch基本一样，不同的不需要在每个Case中添加Break,而是隐藏了Break,当然你可以显示加入break
-//fallthrough 与 break相反, fallthrough将忽略下一个case的判断而直接执行.并且fallthrough必须是当前case的最后一条语句,并且不能放在最后一个case中.
+//fallthrough 与 break相反, fallthrough将忽略下一个case的判断而直接执行.并且fallthrough必须是当前case的最后一条语句,并且不能放在switch最后一句.
 func main() {
     var ch rune
     var cl string
@@ -18,6 +18,7 @@ func main() {
     }
     fmt.Println(cl)
 
+    // fallthrough 忽略下一个case的判断语句 而直接执行
     var i int
     i = 3
     switch {
@@ -37,5 +38,13 @@ func main() {
         fmt.Println("i > 4")
     }
 
+    switch ch {
+        case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+        cl = "Int"
+        case 'A', 'B', 'C', 'D', 'a', 'b', 'c', 'd', 'e':
+        cl = "ABC"
+        default:
+        cl = "Other Char"
+    }
 
 }
