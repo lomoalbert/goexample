7e6cdd5236a076d2934cf6063e1652a03ac661b2
albert <lomoalbert@gmail.com>
2015-06-29 22:48:36 +0800

    add switch

--- switch.go
+++ switch.go
@@ -1,4 +1,4 @@
-package goexample
+package main
 import "fmt"
 //switch是最灵活的一种控制语句,表达式可以不是常量或者字符串，也可以沒有表达式，如果没有表达式则如同if-else-else。
 //一般用法:和其他语言的Switch基本一样，不同的不需要在每个Case中添加Break,而是隐藏了Break,当然你可以显示加入break
@@ -12,21 +12,31 @@ func main() {
         cl = "0"
         case '1':
         cl = "1"
-        break //可以添加
+        break //可以添加,默认自动添加,除非fallthrough
         default:
         cl = "Other Char"
     }
     fmt.Println(cl)
 
+    var i int
+    i = 3
     switch {
-        case ch=='0':
-        cl="0"
-        case ch=='1':
-        cl="1"
-        default:
-        cl="Other Char"
+        case i>0:
+        fmt.Println("i > 0")
+        fallthrough
+        case i>1:
+        fmt.Println("i > 1")
+        fallthrough
+        case i>2:
+        fmt.Println("i > 2")
+        fallthrough
+        case i>3:
+        fmt.Println("i > 3")
+        fallthrough
+        case i>4:
+        fmt.Println("i > 4")
+
     }
-    fmt.Println(cl)
 
 
 }
