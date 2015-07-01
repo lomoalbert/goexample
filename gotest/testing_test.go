4c8bfb5a8c64ff6d1a9fa7ccdb7113e53a5d73c3
albert <lomoalbert@gmail.com>
2015-07-01 22:41:54 +0800

    add fmt

--- gotest/testing_test.go
+++ gotest/testing_test.go
@@ -4,6 +4,22 @@ import (
     "testing"
 )
 
+/*
+使用原则
+
+1. 文件名必须是_test.go结尾的，这样在执行go test的时候才会执行到相应的代码
+2. 你必须import testing这个包
+3. 所有的测试用例函数必须是Test开头
+4. 测试用例会按照源代码中写的顺序依次执行
+5. 测试函数TestXxx()的参数是testing.T，我们可以使用该类型来记录错误或者是
+   测试状态
+6. 测试格式：func TestXxx (t *testing.T),Xxx部分可以为任意的字母数字的组合，
+   但是首字母不能是小写字母[a-z]，例如Testintdiv是错误的函数名。
+7. 函数中通过调用testing.T的Error, Errorf, FailNow, Fatal, FatalIf方法，
+   说明测试不通过，调用Log方法用来记录测试的信息。
+*/
+
+
 //功能测试
 func Test_Division(t *testing.T) {
     if i, e := Division(6, 2); i != 3 || e != nil { //try a unit test on function
