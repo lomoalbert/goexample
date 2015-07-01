89b3a6636c8e4ef93050bf74a4d2f5b264b9272f
albert <lomoalbert@gmail.com>
2015-07-01 22:55:01 +0800

    add fmt

--- fmt.go
+++ fmt.go
@@ -90,7 +90,7 @@ func main() {
     P("%v\n", site) //相应值的默认格式
     P("%+v\n", site) //打印结构体时,显示字段名
     P("%#v\n", site) //相应值的go语法表示
-    P("%T\n", site)
+    P("%T\n", site) // 相应值的类型的go语法表示
     P("%%\n")
 
 }
