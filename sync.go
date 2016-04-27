ce8c415e32501f3243c7b04814158448db2b4ca2
Albert <lomoalbert@users.noreply.github.com>
2016-04-27 11:19:23 +0800

    Update sync.go

--- sync.go
+++ sync.go
@@ -6,22 +6,16 @@ import (
 )
 
 func main() {
-    var wg sync.WaitGroup
+    wg := new(sync.WaitGroup)
     for i := 0; i < 100; i++ {
-        add(wg)
-    }
-
-    for i := 0; i < 100; i++ {
+        wg.Add(1)
         go done(wg)
     }
     wg.Wait()
     fmt.Println("exit")
 }
 
-func add(wg sync.WaitGroup) {
-    wg.Add(1)
-}
-
 func done(wg sync.WaitGroup) {
+    //dosomething
     wg.Done()
 }
